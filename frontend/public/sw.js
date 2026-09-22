ca6ee2bf6d70950e97053b76081d2462f82481bb
Alexis Torres <alexis.torres.alvarez@gmail.com>
2026-09-22 14:25:03 -0300

    Eteria 6: rotate PWA cache

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'eteria-v1';
+const CACHE = 'eteria-v6';
 const CORE = ['/', '/manifest.webmanifest', '/icon.svg'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
